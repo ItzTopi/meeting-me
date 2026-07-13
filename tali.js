58d1ba1f7e49972380c0c853ee2fd5d8a22ec0b7
ItzTopi <toplakbence18@gmail.com>
2026-07-13 20:33:08 +0200

    Add files via upload

---
+++ tali.js
@@ -0,0 +1,21 @@
+
+const button = document.getElementById("button");
+
+button.addEventListener("mouseover", moveButton);
+
+function moveButton() {
+    const maxX = window.innerWidth - button.offsetWidth;
+    const maxY = window.innerHeight - button.offsetHeight;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    button.style.left = randomX + "px";
+    button.style.top = randomY + "px";
+};
+
+
+
+
+
+
